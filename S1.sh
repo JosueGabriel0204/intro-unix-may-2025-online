7732bfd8c8ce3f5baff80a0809e23a940d6d6497
JosueGabriel0204 <jopachaypa@uide.edu.ec>
2026-05-20 02:05:19 +0000

    edit S1

--- S1.sh
+++ S1.sh
@@ -1,5 +1,10 @@
 # uname -r Muestra la nube donde corremos la VM (Virtual Machine)
+
 uname -r
+
 # uname -a Muestra toda la info de mi sistema operativo y el hardware asociado
+
 uname -a
 
+#Realizar un wip "work in progress" siempre al querer guardar los trabajos
+#Commit = Confirmacion / al realizar un wip debera ir con un msj

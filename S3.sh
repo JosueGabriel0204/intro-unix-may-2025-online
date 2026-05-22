d35382344a83b1c2419320fc88a33da769802b76
JosueGabriel0204 <jopachaypa@uide.edu.ec>
2026-05-22 01:48:57 +0000

    wip: s3

---
+++ S3.sh
@@ -0,0 +1,36 @@
+#ls "lista archivos"
+ls
+
+#mkdir "make directory"
+mkdir
+
+# ls --all muestra todos los 
+ls -all
+
+# cd .. me mueve al directorio que contiene al director actual (directorio anterior)
+cd..
+
+#ls -- help
+ls -- help
+
+#touch "sirve para crear un nuevo archivo"
+touch 
+
+# Ruta absoluta
+cd /workspaces/intro-unix-may-2025-online
+
+# cambiar directorio a raiz
+cd /
+
+# rmdir elimina directorios
+rmdir
+
+#rm elimina archivos
+rm 
+
+#cp copy directory 
+cp 
+
+#mv sirve para mover y/o renombrar archivos
+mv 
+

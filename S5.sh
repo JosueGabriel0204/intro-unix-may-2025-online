31cb95188b819c0bc8479b33967574c529a498a4
JosueGabriel0204 <jopachaypa@uide.edu.ec>
2026-06-05 02:28:39 +0000

    S5 commit

---
+++ S5.sh
@@ -0,0 +1,11 @@
+#nos permite dar permisos root a otro usuario "superuser do"
+sudo
+
+#nos permite actualizar las versiones mas recientes de los programas
+sudo apt update
+
+#nos permite actualizar la base de datos de paquetes disponibles en los repositorios del sistema Linux
+sudo apt upgrade
+
+# es cuando se tiene complicaciones con las librerias por la existencia de versiones incompatibles lo cual genera fallos
+dependencyhell

401bed1709ad59f800eddb67a4b4b1907aa7db91
JosueGabriel0204 <jopachaypa@uide.edu.ec>
2026-06-19 01:33:10 +0000

    log en black

--- blackhatbash.sh
+++ blackhatbash.sh
@@ -1,5 +1,12 @@
-#
+#nos imprime las IP
 grep "35.237.4.214\|13.66.139.0" log.txt
 
 
+#imprime la columna seleccionada
+awk '{print $1}' log.txt
 
+#imprime las columnas mencionadas 
+awk '{print $1,$2,$3}' log.txt
+
+#nos imprime la ultima columna 
+awk '{print $1,$NF}' log.txt
